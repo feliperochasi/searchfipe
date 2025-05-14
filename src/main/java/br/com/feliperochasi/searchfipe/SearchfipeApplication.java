9e83ff709c8c534b144366f2a07eaa23277ec751
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-14 11:51:37 -0300

    start project

---
+++ src/main/java/br/com/feliperochasi/searchfipe/SearchfipeApplication.java
@@ -0,0 +1,13 @@
+package br.com.feliperochasi.searchfipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchfipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchfipeApplication.class, args);
+	}
+
+}
