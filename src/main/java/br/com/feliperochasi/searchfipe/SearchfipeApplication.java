31f04f798ab6ca271878b5ffb600ac16e8dd895e
Felipe Rocha <darkkfoxx.z@gmail.com>
2025-05-14 11:59:05 -0300

    Create main and initialize

--- src/main/java/br/com/feliperochasi/searchfipe/SearchfipeApplication.java
+++ src/main/java/br/com/feliperochasi/searchfipe/SearchfipeApplication.java
@@ -1,13 +1,20 @@
 package br.com.feliperochasi.searchfipe;
 
+import br.com.feliperochasi.searchfipe.main.Main;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SearchfipeApplication {
+public class SearchfipeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchfipeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main();
+		main.initMain();
+	}
 }
--- src/main/java/br/com/feliperochasi/searchfipe/main/Main.java
+++ src/main/java/br/com/feliperochasi/searchfipe/main/Main.java
@@ -1,4 +1,8 @@
 package br.com.feliperochasi.searchfipe.main;
 
 public class Main {
+
+    public void initMain() {
+
+    }
 }
